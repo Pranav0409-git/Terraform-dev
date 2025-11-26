20ae60da60fa64e77dc36316bb9bd712d9767766
root <root@ip-172-31-18-204.ec2.internal>
2025-11-26 11:26:24 +0000

    Updated main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MySimpleEC2"
+  }
+}
+
