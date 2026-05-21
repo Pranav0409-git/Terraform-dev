741ab4b675f58dfa99a3cddc1c3546e2dd62c51f
EC2 Default User <ec2-user@ip-172-31-7-214.ec2.internal>
2026-05-21 11:34:41 +0000

    updated TF code

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2
-  instance_type = "t2.micro"
+  ami           = "ami-0236922087fa98b6e" # Amazon Linux 2
+  instance_type = "t3.micro"
 
   tags = {
     Name = "MySimpleEC2"
